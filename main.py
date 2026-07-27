4d92e07612ab3d9196b15c13fe0372be774dbce9
Duuhaan <duhandursunoglu@gmail.com>
2026-07-27 15:28:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("SUP Weather sistemi çalışıyor.")
+
+url = (
+    "https://api.open-meteo.com/v1/forecast"
+    "?latitude=36.8969"
+    "&longitude=30.7133"
+    "&current=temperature_2m,weather_code,wind_speed_10m"
+)
+
+response = requests.get(url)
+data = response.json()
+
+print("📍 Antalya")
+print(f"🌡️ Sıcaklık: {data['current']['temperature_2m']} °C")
+print(f"💨 Rüzgar: {data['current']['wind_speed_10m']} km/s")
+print(f"🌤️ Hava Kodu: {data['current']['weather_code']}")
